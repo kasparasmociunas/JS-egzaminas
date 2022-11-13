c5269976475b32a4366b27d814a95dcef9897ef2
Kasparas <kasparas.mociunas@gmail.com>
2022-11-13 23:08:10 +0200

    8th task done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,37 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  let calculator = new Calculator(11, 4);
+  
+  //!1 Sum
+  Calculator.prototype.sum = function () {
+    return this.a + this.b;
+  };
+  console.log(calculator.sum());
+  
+  //!2 Substraction
+  Calculator.prototype.subtraction = function () {
+    return this.a - this.b;
+  };
+
+  console.log(calculator.subtraction());
+  //! 3 Multiplication
+  Calculator.prototype.multiplication = function () {
+    return this.a * this.b;
+  };
+
+  console.log(calculator.multiplication());
+  //! 4 Division
+  Calculator.prototype.division = function () {
+    return this.a / this.b;
+    
+  };
+  console.log(calculator.division());
